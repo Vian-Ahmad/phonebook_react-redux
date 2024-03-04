e29edd70f016b9d0e0d77089ba59990d6b4b291a
vian <novian701@gmail.com>
2024-03-04 20:51:12 +0700

    adding code

--- phonebook-client/src/components/AvatarForm.js
+++ phonebook-client/src/components/AvatarForm.js
@@ -1,7 +1,29 @@
-import { faArrowLeft, faDoorClosed, faFloppyDisk } from "@fortawesome/free-solid-svg-icons"
+import { faArrowLeft, faFloppyDisk } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import { uploadAvatar } from "../reducers/API"
 
-const AvatarForm = () => {
+const AvatarForm = (user) => {
+    const [selectImage, setSelectImage] = useState()
+
+    const dispatch = useDispatch()
+
+    const imageChange = (e) => {
+        if(e.target.files && e.target.files.length > 0) {
+            setSelectImage(e.target.files[0])
+
+        }
+    }
+
+    useEffect(() => {
+        if(selectImage) {
+            const newAva = new FormData()
+            newAva.append('avatar', selectImage)
+            dispatch(uploadAvatar({id: user.id, formUser: newAva}))
+        }
+    })
+
     return (
         <div className="formAvatar">
             <div className="preview-ava">
